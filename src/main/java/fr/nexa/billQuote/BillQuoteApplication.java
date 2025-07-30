0d9f3870f14cfc7af79433c811c3a173db084a95
odeyacohen <odeyacohen05@gmail.com>
2025-07-30 12:20:02 +0200

    modif juillet

---
+++ src/main/java/fr/nexa/billQuote/BillQuoteApplication.java
@@ -0,0 +1,13 @@
+package fr.nexa.billQuote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillQuoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BillQuote/billQuote/BillQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package BillQuote.billQuote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillQuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
